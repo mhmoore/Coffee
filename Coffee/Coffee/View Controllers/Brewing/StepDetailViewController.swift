229a8a0c10f811719005ced1266e572becd5a74d
Mike Moore <michaelhmoore1@gmail.com>
2019-09-23 23:53:42 -0600

    initial custom guide setup

---
+++ Coffee/Coffee/View Controllers/Brewing/StepDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  StepDetailViewController.swift
+//  Coffee
+//
+//  Created by Michael Moore on 9/23/19.
+//  Copyright © 2019 Michael Moore. All rights reserved.
+//
+
+import UIKit
+
+class StepDetailViewController: UIViewController {
+
+    var step: Step?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
